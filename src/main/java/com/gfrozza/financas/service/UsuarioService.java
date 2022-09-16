99c0a0c5dcd2aaf1a12097773b0104353e50d7e6
Giovanni Frozza <giovanni_cruz@terceiros.sicredi.com.br>
2022-09-16 18:18:34 -0300

    implementado service do usuario

--- src/main/java/com/gfrozza/financas/service/UsuarioService.java
+++ src/main/java/com/gfrozza/financas/service/UsuarioService.java
@@ -1,2 +1,14 @@
-package com.gfrozza.financas.service;public class UsuarioService {
+package com.gfrozza.financas.service;
+
+import com.gfrozza.financas.entity.Usuario;
+
+public interface UsuarioService {
+
+    Usuario autenticar(String email, String senha);
+
+    Usuario salvarUsuario(Usuario usuario);
+
+    void validarEmail(String email);
+
+
 }
