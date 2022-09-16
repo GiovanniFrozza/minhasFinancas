6fdb47e18b67097f2150a87e1444d17581ee1a94
Giovanni Frozza <giovanni_cruz@terceiros.sicredi.com.br>
2022-09-16 18:18:52 -0300

    interface do usuario service

--- src/main/java/com/gfrozza/financas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/gfrozza/financas/service/impl/UsuarioServiceImpl.java
@@ -1,2 +1,29 @@
-package com.gfrozza.financas.service.impl;public class UsuarioServiceImpl {
+package com.gfrozza.financas.service.impl;
+
+import com.gfrozza.financas.entity.Usuario;
+import com.gfrozza.financas.repository.UsuarioRepository;
+import com.gfrozza.financas.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public Usuario autenticar(String email, String senha) {
+        return null;
+    }
+
+    @Override
+    public Usuario salvarUsuario(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public void validarEmail(String email) {
+
+    }
 }
