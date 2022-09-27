41d720e4dff97497bf72b96b087644a0b769e942
giovanni <giovannifrozza470@gmail.com>
2022-09-26 21:19:39 -0300

    testes do usuario na service

--- src/test/java/com/gfrozza/financas/model/service/impl/UsuarioServiceImplTest.java
+++ src/test/java/com/gfrozza/financas/model/service/impl/UsuarioServiceImplTest.java
@@ -1,5 +1,42 @@
 package com.gfrozza.financas.model.service.impl;
 
 
+import com.gfrozza.financas.model.entity.Usuario;
+import com.gfrozza.financas.model.exceptions.RegraNegocioException;
+import com.gfrozza.financas.model.repository.UsuarioRepository;
+import com.gfrozza.financas.model.service.UsuarioService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 public class UsuarioServiceImplTest {
+
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Test
+    public void validarEmailTest() {
+        usuarioRepository.deleteAll();
+        usuarioService.validarEmail("email@email.com");
+    }
+
+    @Test(expected = RegraNegocioException.class)
+    public void validaEmailJaCadastradoTest() {
+        Usuario usuario = Usuario.builder()
+                .email("mail@email.com")
+                .nome("nome")
+                .build();
+        usuarioRepository.save(usuario);
+
+        usuarioService.validarEmail(usuario.getEmail());
+    }
+
 }
