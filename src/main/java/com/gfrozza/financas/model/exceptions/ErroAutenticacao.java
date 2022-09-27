28f84f87db0ca3c2f4d718f68c042f6601cb1e21
giovanni <giovannifrozza470@gmail.com>
2022-09-26 21:23:21 -0300

    classe de erroAutenticacao exception

---
+++ src/main/java/com/gfrozza/financas/model/exceptions/ErroAutenticacao.java
@@ -0,0 +1,8 @@
+package com.gfrozza.financas.model.exceptions;
+
+public class ErroAutenticacao extends RuntimeException {
+
+    public ErroAutenticacao(String mensagem) {
+        super(mensagem);
+    }
+}
