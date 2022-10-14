2427bff2337f0c246bdfec7df49f5c7a00c469a8
Giovanni Frozza <giovanni_cruz@terceiros.sicredi.com.br>
2022-10-14 11:06:29 -0300

    merge

--- src/test/java/com/gfrozza/financas/model/service/impl/UsuarioServiceImplTest.java
+++ src/test/java/com/gfrozza/financas/model/service/impl/UsuarioServiceImplTest.java
@@ -2,6 +2,7 @@ package com.gfrozza.financas.model.service.impl;
 
 
 import com.gfrozza.financas.model.entity.Usuario;
+import com.gfrozza.financas.model.exceptions.ErroAutenticacaoException;
 import com.gfrozza.financas.model.exceptions.RegraNegocioException;
 import com.gfrozza.financas.model.repository.UsuarioRepository;
 import com.gfrozza.financas.model.service.UsuarioService;
@@ -43,6 +44,12 @@ public class UsuarioServiceImplTest {
     @Test(expected = RegraNegocioException.class)
     public void validaEmailJaCadastradoTest() {
         when(usuarioRepository.existsByEmail(anyString())).thenReturn(true);
+        usuarioService.validarEmail("email@email.com");
+    }
+
+    @Test(expected = ErroAutenticacaoException.class)
+    public void autenticarTest() {
+        when(usuarioRepository.findByEmail(anyString())).thenReturn(UsuarioStub.usuarioCreate());
         usuarioService.validarEmail("email@email.com");
     }
 
