6c29fa00b4fd1f9088300392ef5e130296b7932b
giovanni <giovannifrozza470@gmail.com>
2022-10-16 11:05:22 -0300

    metodo de buscar

--- src/main/java/com/gfrozza/financas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/gfrozza/financas/service/impl/LancamentoServiceImpl.java
@@ -5,10 +5,14 @@ import com.gfrozza.financas.model.enums.StatusLancamentoEnum;
 import com.gfrozza.financas.model.repository.LancamentoRepository;
 import com.gfrozza.financas.service.LancamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class LancamentoServiceImpl implements LancamentoService {
@@ -23,22 +27,33 @@ public class LancamentoServiceImpl implements LancamentoService {
     }
 
     @Override
+    @Transactional
     public Lancamento atualizar(Lancamento lancamento) {
-        return null;
+        Objects.requireNonNull(lancamento.getId());
+        return this.lancamentoRepository.save(lancamento);
     }
 
     @Override
     public void deletar(Lancamento lancamento) {
-
+        Objects.requireNonNull(lancamento.getId());
+        this.lancamentoRepository.delete(lancamento);
     }
 
     @Override
+    @Transactional
     public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-        return null;
+        /*Concatena a busca com base nas informações que tem no lancamento.*/
+        Example<Lancamento> example = Example.of(lancamentoFiltro,
+                ExampleMatcher.matching()
+                        .withIgnoreCase()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+
+        return this.lancamentoRepository.findAll(example);
     }
 
     @Override
     public void atualizarStatus(Lancamento lancamento, StatusLancamentoEnum statusLancamentoEnum) {
-
+        lancamento.setStatusLancamentoEnum(statusLancamentoEnum);
+        atualizar(lancamento);
     }
 }
